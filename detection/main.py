840e5de59e71879ae0ce2b9b8133c786c9442650
Dorian Bucaille <74455265+dorian-bucaille@users.noreply.github.com>
2021-02-05 17:58:05 +0100

    Added player tracking and seconds since last seen.

--- detection/main.py
+++ detection/main.py
@@ -2,6 +2,7 @@ import cv2 as cv
 from visionhsv import Vision
 from hsvfilter import HsvFilter
 from gamecapture import GameCapture
+from time import time
 
 # This script would not exist without the excellent work of Ben from Learn Code By Gaming.
 # Go check him out ! https://www.youtube.com/c/LearnCodeByGaming
@@ -25,6 +26,9 @@ rectangles = {}
 points = {}
 thresholds = {}
 positions = {}
+timings = {}
+last_seen = {}
+current_room = None
 
 # Initialize player trackers
 for color in colors:
@@ -34,6 +38,8 @@ for color in colors:
     points.update({color: None})
     thresholds.update({color: 0.5})
     positions.update({color: None})
+    timings.update({color: None})
+    last_seen.update({color: None})
 
 # Initialize room trackers
 for room in rooms:
@@ -73,18 +79,19 @@ filters = {
 }
 
 while True:
-
+    current_room = None
     # filters["room"] = trackers["room"].get_hsv_filter_from_controls()
 
     # Get current frame and resize it so the process is faster
     screenshot = amongus_screenshot.get_screenshot(256, 144)
 
-    # [164:173, 70:188]
     # Detect room
     for room in rooms:
         processed_images[room] = trackers[room].apply_hsv_filter(screenshot, filters["room"])
         rectangles[room] = trackers[room].find(processed_images[room], threshold=thresholds["room"], max_results=1)
         points[room] = trackers[room].get_click_points(rectangles[room])
+        if len(points[room]):
+            current_room = room
 
     # Get detected points for every color
     for color in colors:
@@ -92,12 +99,10 @@ while True:
         rectangles[color] = trackers[color].find(processed_images[color], threshold=thresholds[color], max_results=1)
         points[color] = trackers[color].get_click_points(rectangles[color])
 
-        # Draw a cross and write the name of the room
-    for room in rooms:
-        trackers[room].draw_crosshairs(screenshot, points[room], (255, 0, 255))
-        if len(points[room]):
-            cv.putText(screenshot, room, (points[room][0][0] + 1, points[room][0][1] - 1),
-                       cv.FONT_HERSHEY_PLAIN, 0.7, (255, 0, 255), 1, cv.LINE_AA)
+        # Update player location
+        if len(points[color]):
+            positions[color] = current_room
+            timings[color] = time()
 
     # Draw a cross and write the color detected for every color
     for color in colors:
@@ -112,7 +117,29 @@ while True:
     # Display result image
     cv.imshow('Tracking screen', cv.resize(screenshot, (1280, 720)))
 
-    # Press 'q' with the output window focused to exit.
-    if cv.waitKey(1) == ord('q'):
-        cv.destroyAllWindows()
+    # Meeting detected
+    # Press 'a' to simulate this scenario
+    if cv.waitKey(1) == ord('a'):
+        time_now = time()
+        for color in colors:
+            if timings[color]:
+                last_seen[color] = time_now - timings[color]
+        print(last_seen)
         break
+
+# Print last positions of encountered players.
+for color in colors:
+    if last_seen[color] and positions[color]:
+        if int(last_seen[color]) == 0 or int(last_seen[color]) == 1:
+            print(f'{color} last seen {int(last_seen[color])} second ago in {positions[color]}.')
+        else:
+            print(f'{color} last seen {int(last_seen[color])} seconds ago in {positions[color]}.')
+    elif last_seen[color]:
+        if int(last_seen[color]) == 0 or int(last_seen[color]) == 1:
+            print(f'{color} last seen {int(last_seen[color])} second ago.')
+        else:
+            print(f'{color} last seen {int(last_seen[color])} seconds ago.')
+    else:
+        print(f'{color} not seen.')
+
+print('Done.')
